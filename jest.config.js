f31ecee6a3add55b0790db78ff103886215f3fb5
Alcir Junior <junior@cajucomunica.com.br>
2022-08-24 15:37:51 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- jest.config.js
+++ jest.config.js
@@ -91,7 +91,9 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '^domain$': '<rootDir>/src/index.ts'
+    '^domain$': '<rootDir>/src/domain/index.ts',
+    '^data$': '<rootDir>/src/data/index.ts',
+    '^protocols$': '<rootDir>/src/protocols/index.ts'
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
---
+++ src/data/protocols/http/http-post-client.ts
@@ -0,0 +1,3 @@
+export interface HttpPostClient {
+  post: (utl: string) => Promise<void>
+}
