064cc37bc2acaa20b5c83355cefeec12a1622914
Alcir Junior <junior@cajucomunica.com.br>
2022-08-30 08:40:55 -0300

    test: ensure AxiosHttpClient calls axios with correct URL and verb and values

--- src/infra/http/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { faker } from '@faker-js/faker'
 import { AxiosHttpClient } from '@infra/http'
+import { HttpPostParams } from '@data/protocols'
 
 jest.mock('axios')
 
@@ -10,13 +11,18 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.science.chemicalElement()
+})
+
 describe('AxiosHttpClient', () => {
-  it('should call axios with correct URL and Verb', async () => {
-    const url = faker.internet.url()
+  it('should call axios with correct URL and Verb and values', async () => {
+    const request = mockPostRequest()
     const sut = makeSut()
 
-    await sut.post({ url })
+    await sut.post(request)
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
 })
