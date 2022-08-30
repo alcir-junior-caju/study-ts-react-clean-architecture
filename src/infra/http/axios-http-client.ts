ac18c1e83d35e35b6a641b45ab1f75a7f2fd3a52
Alcir Junior <junior@cajucomunica.com.br>
2022-08-30 08:40:15 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL and verb and values

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -3,6 +3,6 @@ import { HttpPostParams } from '@data/protocols'
 
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
   }
 }
